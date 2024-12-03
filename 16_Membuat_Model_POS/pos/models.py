a7e503759828c34170d7e30a8b1f2ea9d4fb3d50
roni.purwanto@amartha.com <roni.purwanto@amartha.com>
2024-12-03 13:12:28 +0700

    feat: update model

--- 16_Membuat_Model_POS/pos/models.py
+++ 16_Membuat_Model_POS/pos/models.py
@@ -5,6 +5,9 @@ class Category(models.Model):
     code = models.CharField(max_length=20, default='C001')
     name = models.CharField(max_length=255)
 
+    def __str__(self):
+        return "".format(self.name, +self.code)
+
 class Product(models.Model):
     name = models.CharField(max_length=255)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
@@ -12,17 +15,28 @@ class Product(models.Model):
     stock = models.PositiveIntegerField()
     description = models.TextField(blank=True)  # Deskripsi produk (opsional)
 
+    def __str__(self):
+        return "{}-{}".format(self.id, self.name)
+
 class Customer(models.Model):
     name = models.CharField(max_length=255)
     member = models.BooleanField(default=False)  # Apakah pelanggan member?
+
+    def __str__(self):
+        return "{}-{}".format(self.id, self.name)
 
 class Transaction(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank=True)  # Pelanggan (opsional)
     products = models.ManyToManyField(Product, through='TransactionItem')  # Produk-produk dalam transaksi
     total_amount = models.DecimalField(max_digits=10, decimal_places=2)
     transaction_date = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return "{}".format(self.id)
 
 class TransactionItem(models.Model):
     transaction = models.ForeignKey(Transaction, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField()
+
+    def __str__(self):
+        return "{}-{}-{}".format(self.id, self.product.name, self.quantity)
