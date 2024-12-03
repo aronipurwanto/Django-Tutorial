2cd7d655a47b778edfda76a93d282a352fbe31c4
roni.purwanto@amartha.com <roni.purwanto@amartha.com>
2024-12-03 14:47:26 +0700

    feat: fix url

--- 16_Membuat_Model_POS/pos/urls.py
+++ 16_Membuat_Model_POS/pos/urls.py
@@ -3,7 +3,7 @@ from django.urls import re_path, path
 from pos import views
 
 urlpatterns = [
-    path('category/', views.categoryList),
-    path('product/', views.product_list),
-    path('product/add', views.product_add),
+    path('category/', views.categoryList, name='categoryList'),
+    path('product/', views.product_list, name='productList'),
+    path('product/add', views.product_add, name='product_add'),
 ]
